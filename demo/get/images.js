d3cee998ae3aafbac6482898d68f4e3edc52f9c0
Lars Kappert <lars@webpro.nl>
2013-02-02 20:16:10 +0100

    Added image.base64 and image.src generators (incl. demo at /images)

---
+++ demo/get/images.js
@@ -0,0 +1,23 @@
+var g = require('../../lib/generators');
+
+module.exports = {
+	path: '/images',
+	template: {
+        imgBase64: function() {
+            return g.image.base64({width:200, height: 200});
+        },
+        imgBase64_custom: function() {
+            return g.image.base64({
+                host: 'http://lorempixel.com',
+                path: '/150/150/abstract/' + g.random(10)
+            });
+        },
+        imgSrc: function() {
+            return g.image.src({width:150, height: 150});
+        },
+        imgSrc_custom: function() {
+            return 'http://placekitten.com/200/300';
+        },
+        status: 'OK'
+    }
+};
