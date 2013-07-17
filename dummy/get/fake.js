7efd379b422bae83bddf9b2373ff923705824a98
Lars Kappert <lars@webpro.nl>
2013-07-18 00:07:30 +0200

    Commented out endpoints that would potentially give errors for new users

---
+++ dummy/get/fake.js
@@ -0,0 +1,19 @@
+// Please install dyson-generators before uncommenting this endpoint: npm install dyson-generators --save-dev
+
+/*
+var g = require('dyson-generators');
+
+module.exports = {
+	path: '/fake',
+	template: {
+		id: g.id,
+        name: g.name,
+        address: {
+            city: g.address.city,
+            zipUS: g.address.zipUS
+        },
+        time: g.time.byQuarter,
+        lorem: g.lorem.short,
+    }
+};
+*/
