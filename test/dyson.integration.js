88c2e0394c090bfcc1964f23ad9fe94bf666fef0
29decibel <mike.d.1984@gmail.com>
2014-03-20 23:45:34 -0700

    add requireParameters config option, return 400 bad request if required parameter not found in query/body/paras

--- test/dyson.integration.js
+++ test/dyson.integration.js
@@ -48,6 +48,9 @@ describe('dyson', function() {
                 }, {
                     path: '/combined/:id',
                     template: {}
+                }, {
+                    path: '/require-para',
+                    requireParameters: ['name']
                 }]
             };
 
@@ -109,14 +112,27 @@ describe('dyson', function() {
 
         });
 
+
         it('should respond with a 204 for an OPTIONS request', function(done) {
 
             request(app).options('/').expect(204).end(function(err, res) {
                 res.headers['access-control-allow-methods'].should.equal('GET,HEAD,PUT,POST,DELETE');
                 res.headers['access-control-allow-credentials'].should.equal('true');
-				// The next actual value is 'undefined', should be req.header('Origin') (probably an issue with supertest)
-				// res.headers['access-control-allow-origin'].should.equal('*');
+                // The next actual value is 'undefined', should be req.header('Origin') (probably an issue with supertest)
+                // res.headers['access-control-allow-origin'].should.equal('*');
                 done();
+            });
+
+        });
+
+        it('should respond with 400 bad request if required parameter not found', function(done) {
+            request(app).get('/require-para').expect(400).end(function(err, res) {
+                res.body.error.should.include("(name) not found");
+
+                // with required parameter
+                request(app).get('/require-para?name=Tim').expect(200).end(function(err, res) {
+                    done();
+                });
             });
 
         });
