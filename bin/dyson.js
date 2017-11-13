4bb706b539fd775d653d20def68ec3767c17db98
Lars Kappert <lars@webpro.nl>
2017-11-14 00:06:54 +0100

    Rewrite bin

--- bin/dyson.js
+++ bin/dyson.js
@@ -1,5 +1,44 @@
 #!/usr/bin/env node
 
-const cli = require('../lib/cli');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const pkg = require('../package.json');
+const { bootstrap } = require('../lib/dyson');
 
-cli.execute(process.argv.slice(2));
+const [dir, port] = process.argv.slice(2);
+
+const showHelpAndExit = () => {
+  console.info(`dyson v${pkg.version}`);
+  console.info('Usage: dyson <dir> [port]');
+  process.exit(1);
+};
+
+if (dir) {
+  let localOpts;
+
+  const configPath = path.join(process.cwd(), dir);
+  const dirStat = fs.statSync(configPath);
+
+  if (!dirStat || !dirStat.isDirectory()) {
+    showHelpAndExit();
+  }
+
+  try {
+    localOpts = require(path.join(configPath, 'dyson.json'));
+  } catch (err) {
+    localOpts = {};
+  }
+
+  const opts = _.defaults(localOpts, {
+    port: port || 3000,
+    configDir: dir,
+    proxy: false,
+    multiRequest: ',',
+    quiet: false
+  });
+
+  bootstrap(opts);
+} else {
+  showHelpAndExit();
+}
