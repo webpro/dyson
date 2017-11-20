7aa35b189ec6cddf3b9d49093664f39d236479c7
Lars Kappert <lars@webpro.nl>
2017-11-20 21:05:00 +0100

    Move app refs

--- test/_helpers.js
+++ test/_helpers.js
@@ -1,9 +1,6 @@
 import dyson from '../lib/dyson';
-import { logger } from '../lib/util';
 
 export const getService = (config, options = {}) => {
   const app = dyson.createServer(options);
-  app.set('dyson_options', options);
-  app.set('dyson_logger', logger({ quiet: true }));
   return config ? dyson.registerServices(app, options, config) : app;
 };
